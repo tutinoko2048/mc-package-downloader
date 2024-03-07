484b44b35f36a11d0b40d3f181eb1731e88582a9
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2024-03-07 01:30:26 +0000

    feat: version list

---
+++ app/downloader.ts
@@ -0,0 +1,33 @@
+
+// 1.4.0-beta.1.20.10-stable 1.0.0-beta.release.1.19.40
+const stableBetaRegex = /\d+\.\d+\.\d+-beta\.(?:\d+\.\d+\.\d+-stable|release\.\d+\.\d+\.\d+)/
+// 1.6.0-beta.1.20.30-preview.20 1.0.0-beta.preview.1.19.50.20
+const previewBetaRegex = /\d+\.\d+\.\d+-beta\.(?:\d+\.\d+\.\d+-preview\.\d+|preview\.\d+\.\d+\.\d+\.\d+)/
+const previewRcRegex = /\d+\.\d+\.\d+-rc\.\d+\.\d+\.\d+-preview\.\d+/
+const releaseRegex = /^\d+\.\d+\.\d+$/
+
+function sortVersion(a: string, b: string) {
+  if (
+    a.match(/release|rc|\.preview\./) || b.match(/release|rc|\.preview\./)
+  ) {
+    const va = a.match(/\d+\.\d+\.\d+/g)![1];
+    const vb = b.match(/\d+\.\d+\.\d+/g)![1];
+    const vaPreview = a.match(/\d{2}$/g);
+    const vbPreview = b.match(/\d{2}$/g);
+    if (!va || !vb) return a.localeCompare(b);
+    const versionA = va + (vaPreview ? `.${vaPreview[0]}` : '');
+    const versionB = vb + (vbPreview ? `.${vbPreview[0]}` : '');
+    return versionA.localeCompare(versionB);
+  }
+  return a.localeCompare(b);
+}
+
+export async function getPackageVersions(packageName: string) {
+  const res = await fetch(`https://registry.npmjs.org/@minecraft/${packageName}`)
+  const data = await res.json();
+  const versions = Object.keys(data.versions)
+    .filter(x => !x.includes('internal'))
+    .sort(sortVersion)
+    .reverse();
+  return versions;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,41 @@
+'use client'
 
-export default function Main() {
+import { Select, SelectItem, Spinner } from '@nextui-org/react';
+import React from 'react';
+import { getPackageVersions } from './downloader';
+
+const packages = [
+  { name: 'server', label: '@minecraft/server' },
+  { name: 'server-ui', label: '@minecraft/server-ui' },
+  { name: 'vanilla-data', label: '@minecraft/vanilla-data' },
+  { name: 'math', label: '@minecraft/math' },
+  { name: 'server-net', label: '@minecraft/gametest' },
+]
+
+export default function Page() {
+  const [packageName, setPackageName] = React.useState<string>('');
+  const [packageLoading, setPackageLoading] = React.useState<boolean>(false);
+  const [packageVersions, setPackageVersions] = React.useState<string[]>([]);
+
+  const onChangePackage = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    setPackageName(value);
+    if (value) setPackageLoading(true);
+    setPackageVersions([]);
+    const versions = await getPackageVersions(value);
+    setPackageVersions(versions)
+    setPackageLoading(false);
+  }
+
   return (
     <main>
-      Hello World!
+      <h1>Minecraft Package Downloader</h1>
+      <br />
+      <Select required items={packages} label='Select package' size='sm' selectedKeys={[packageName]} onChange={onChangePackage}>
+        {packages.map(mcpackage => <SelectItem key={mcpackage.name}>{`@minecraft/${mcpackage.name}`}</SelectItem>)}
+      </Select>
+      {packageLoading && <Spinner />}
+      {...packageVersions.map(v => <div key={v}>{v}</div>)}
     </main>
   )
 }
