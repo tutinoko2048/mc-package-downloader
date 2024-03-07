51a99f62d14a0d792a0e211dcdbf0dd3e86f8857
RetoRuto9900K <41989402+tutinoko2048@users.noreply.github.com>
2024-03-07 03:09:05 +0000

    Implemented type filter, improve sorting

--- app/downloader.ts
+++ app/downloader.ts
@@ -1,35 +1,39 @@
+const releaseRegex = /^\d+\.\d+\.\d+$/;
 
-// 1.4.0-beta.1.20.10-stable 1.0.0-beta.release.1.19.40
-const stableBetaRegex = /\d+\.\d+\.\d+-beta\.(?:\d+\.\d+\.\d+-stable|release\.\d+\.\d+\.\d+)/
-// 1.6.0-beta.1.20.30-preview.20 1.0.0-beta.preview.1.19.50.20
-const previewBetaRegex = /\d+\.\d+\.\d+-beta\.(?:\d+\.\d+\.\d+-preview\.\d+|preview\.\d+\.\d+\.\d+\.\d+)/
-const previewRcRegex = /\d+\.\d+\.\d+-rc\.\d+\.\d+\.\d+-preview\.\d+/
-const releaseRegex = /^\d+\.\d+\.\d+$/
+const typeList = {
+  full: {
+    'stable-beta': (version: string) => version.includes('beta') && (version.includes('stable') || version.includes('release')),
+    'release': (version: string) => releaseRegex.test(version),
+    'preview-beta': (version: string) => version.includes('preview') && version.includes('beta'),
+    'preview-rc': (version: string) => version.includes('preview') && version.includes('rc')
+  },
+  preview: {
+    'stable': (version: string) => !version.includes('preview'),
+    'preview': (version: string) => version.includes('preview')
+  },
+  stable: {
+    'stable': (_version: string) => true
+  }
+}
+
+export const packages: Record<Packages, { typeList: Record<string, (version: string) => boolean> }> = {
+  'server': { typeList: typeList.full },
+  'server-ui': { typeList: typeList.full },
+  'vanilla-data': { typeList: typeList.preview },
+  'math': { typeList: typeList.stable },
+  'server-net': { typeList: typeList.full }
+}
 
 export type Packages = 'server' | 'server-ui' | 'vanilla-data' | 'math' | 'server-net';
 
-function sortVersion(a: string, b: string) {
-  if (
-    a.match(/release|rc|\.preview\./) || b.match(/release|rc|\.preview\./)
-  ) {
-    const va = a.match(/\d+\.\d+\.\d+/g)![1];
-    const vb = b.match(/\d+\.\d+\.\d+/g)![1];
-    const vaPreview = a.match(/\d{2}$/g);
-    const vbPreview = b.match(/\d{2}$/g);
-    if (!va || !vb) return a.localeCompare(b);
-    const versionA = va + (vaPreview ? `.${vaPreview[0]}` : '');
-    const versionB = vb + (vbPreview ? `.${vbPreview[0]}` : '');
-    return versionA.localeCompare(versionB);
-  }
-  return a.localeCompare(b);
-}
-
-export async function getPackageVersions(packageName: Packages) {
+export async function getPackageVersions(packageName: Packages, type: string) {
+  console.log(packageName, type)
   const res = await fetch(`https://registry.npmjs.org/@minecraft/${packageName}`)
   const data = await res.json();
+  const filter = packages[packageName].typeList[type];
   const versions = Object.keys(data.versions)
     .filter(x => !x.includes('internal'))
-    .sort(sortVersion)
+    .filter(filter)
     .reverse();
   return versions;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,35 @@
-'use client'
+'use client';
 
-import { Select, SelectItem, Spinner } from '@nextui-org/react';
+import { Select, SelectItem, Spacer, Spinner } from '@nextui-org/react';
 import React from 'react';
-import { Packages, getPackageVersions } from './downloader';
-
-const packages: { name: Packages, label: string }[] = [
-  { name: 'server', label: '@minecraft/server' },
-  { name: 'server-ui', label: '@minecraft/server-ui' },
-  { name: 'vanilla-data', label: '@minecraft/vanilla-data' },
-  { name: 'math', label: '@minecraft/math' },
-  { name: 'server-net', label: '@minecraft/gametest' },
-]
+import { Packages, getPackageVersions, packages } from './downloader';
 
 export default function Page() {
-  const [packageName, setPackageName] = React.useState<Packages | ''>('');
+  const [packageName, setPackageName] = React.useState<Packages>('server');
   const [packageLoading, setPackageLoading] = React.useState<boolean>(false);
   const [packageVersions, setPackageVersions] = React.useState<string[]>([]);
+  const [packageType, setPackageType] = React.useState<string>('');
 
   const onChangePackage = async (e: React.ChangeEvent<HTMLSelectElement>) => {
     const value = e.target.value as Packages;
+    const type = packageType in packages[value].typeList ? packageType : Object.keys(packages[value].typeList)[0]
     setPackageName(value);
-    if (value) setPackageLoading(true);
+    setPackageType(type)
+    setPackageLoading(true);
+    await updateVersions(value, type);
+  }
+
+  const onChangeType = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value || Object.keys(packages[packageName].typeList)[0];
+    setPackageType(value);
+    setPackageLoading(true);
+    await updateVersions(packageName, value);
+  }
+
+  const updateVersions = async (packageName: Packages, type: string) => {
     setPackageVersions([]);
-    const versions = await getPackageVersions(value);
-    setPackageVersions(versions)
+    const versions = await getPackageVersions(packageName, type);
+    setPackageVersions(versions);
     setPackageLoading(false);
   }
 
@@ -31,9 +37,17 @@ export default function Page() {
     <main>
       <h1>Minecraft Package Downloader</h1>
       <br />
-      <Select required items={packages} label='Select package' size='sm' selectedKeys={[packageName]} onChange={onChangePackage}>
-        {packages.map(mcpackage => <SelectItem key={mcpackage.name}>{`@minecraft/${mcpackage.name}`}</SelectItem>)}
-      </Select>
+      <div className='horizontal'>
+        <Select required label='Select package' size='sm' selectedKeys={[packageName]} onChange={onChangePackage}>
+          {Object.keys(packages).map(mcpackage => <SelectItem key={mcpackage}>{`@minecraft/${mcpackage}`}</SelectItem>)}
+        </Select>
+        <Spacer x={1} y={1} />
+        <Select required label='Select type' size='sm' selectedKeys={[packageType]} onChange={onChangeType}>
+          {Object.keys(packages[packageName].typeList).map(type => <SelectItem key={type}>{type}</SelectItem>)}
+        </Select>
+      </div>
+      
+      <Spacer y={4} />
       {packageLoading && <Spinner />}
       {...packageVersions.map(v => <div key={v}>{v}</div>)}
     </main>
